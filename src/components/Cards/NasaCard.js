c5ed839f71d7b233a49fdcc6d40cb7fff07f1b2a
sydinh <sydinh.dev@gmail.com>
2019-01-19 18:42:34 +0700

    add more styles :fire:

--- src/components/Cards/NasaCard.js
+++ src/components/Cards/NasaCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Button from 'components/Shared/Button';
+
 const NasaCard = props => (
   <figure className="nasa-card">
     {props.imgUrl && (
@@ -19,6 +21,18 @@ const NasaCard = props => (
       {props.title && <h3 className="nasa-card__caption-title">{props.title}</h3>}
       {props.description && <p className="nasa-card__caption-description">{props.description}</p>}
     </figcaption>
+    {props.isInCollection ? (
+      <Button
+        onClick={props.onDeleteFromCollection}
+        className="button--primary nasa-card__button-delete"
+      >
+        Delete from NASA collection
+      </Button>
+    ) : (
+      <Button onClick={props.onAddToCollection} className="button--primary nasa-card__button-add">
+        Add to NASA collection
+      </Button>
+    )}
   </figure>
 );
 
@@ -28,6 +42,9 @@ NasaCard.propTypes = {
   date: PropTypes.string,
   title: PropTypes.string,
   description: PropTypes.string,
+  isInCollection: PropTypes.bool,
+  onAddToCollection: PropTypes.func,
+  onDeleteFromCollection: PropTypes.func,
 };
 
 NasaCard.defaultProps = {
@@ -36,6 +53,7 @@ NasaCard.defaultProps = {
   date: '',
   title: '',
   description: '',
+  isInCollection: false,
 };
 
 export default NasaCard;
