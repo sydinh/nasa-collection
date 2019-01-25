842d73251f328c9a6d0067dee72167eae641820c
sydinh <sydinh.dev@gmail.com>
2019-01-25 13:57:24 +0700

    rename historyUtil.js to history.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import 'styles/main.scss';
 import store from 'configureStore';
-import history from 'utils/historyUtil';
+import history from 'utils/history';
 import App from 'App';
 
 ReactDOM.render(
